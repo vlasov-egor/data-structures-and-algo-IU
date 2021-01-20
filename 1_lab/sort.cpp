a24b3a3eddda41acacaaff322fe4653f82d5bd62
Egor Vlasov <vlasov05102002@gmail.com>
2021-01-20 23:19:17 +0300

    added 1 draft of sort in 1 lab(cf round)

---
+++ 1_lab/sort.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int n;
+    cin >> n;
+
+    int a[n];
+
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> a[i];
+    }
+
+    for (int i = 1; i < n; ++i)
+    {
+        int key = a[i];
+        int j = i - 1;
+
+        while (a[j] > key && j > 0)
+        {
+            a[j + 1] = a[j];
+            j--;
+        }
+
+        a[j + 1] = key;
+    }
+
+    for (int i = 0; i < n; ++i)
+    {
+        cout << a[i] << " ";
+    }
+
+    return 0;
+}
