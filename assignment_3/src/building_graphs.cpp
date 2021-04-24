4fac3232127126a5ce2d745193c159dcad6b12f8
Egor Vlasov <vlasov05102002@gmail.com>
2021-04-24 15:30:17 +0300

    1 task completed

--- assignment_3/src/building_graphs.cpp
+++ assignment_3/src/building_graphs.cpp
@@ -14,6 +14,11 @@
 
 using namespace std;
 
+/**
+ * @brief vertex class
+ * 
+ * @tparam V type of vertex value
+ */
 template <class V>
 class Vertex
 {
@@ -26,6 +31,12 @@ public:
     }
 };
 
+/**
+ * @brief edge class
+ * 
+ * @tparam E type of edge weight
+ * @tparam V type of vertex value
+ */
 template <class E, class V>
 class Edge
 {
@@ -43,49 +54,97 @@ public:
     }
 };
 
+/**
+ * @brief interface of graph
+ * 
+ * @tparam E type of edge weight
+ * @tparam V type of vertex value
+ */
 template <class E, class V>
 class Graph_ADT
 {
     virtual Vertex<V> *addVertex(V value) = 0;
-    virtual void removeVertex(Vertex<V> vertex) = 0;
+    virtual void removeVertex(Vertex<V> *vertex) = 0;
     virtual Edge<E, V> *addEdge(Vertex<V> *from, Vertex<V> *to, E weight) = 0;
-    virtual void removeEdge(Edge<E, V> edge) = 0;
-    virtual vector<Edge<E, V>> edgesFrom(Vertex<V> vertex) = 0;
-    virtual vector<Edge<E, V>> edgesTo(Vertex<V> vertex) = 0;
-    virtual Vertex<V> findVertex(V value) = 0;
-    virtual Edge<E, V> findEdge(V from_value, V to_value) = 0;
-    virtual bool hasEdge(Vertex<V> v, Vertex<V> u) = 0;
+    virtual void removeEdge(Edge<E, V> *edge) = 0;
+    virtual vector<Edge<E, V> *> edgesFrom(Vertex<V> *vertex) = 0;
+    virtual vector<Edge<E, V> *> edgesTo(Vertex<V> *vertex) = 0;
+    virtual Vertex<V> *findVertex(V value) = 0;
+    virtual Edge<E, V> *findEdge(V from_value, V to_value) = 0;
+    virtual bool hasEdge(Vertex<V> *v, Vertex<V> *u) = 0;
 };
 
+/**
+ * @brief graph class
+ * 
+ * @tparam E type of edge weight
+ * @tparam V type of vertex value
+ */
 template <class E, class V>
 class AdjacencyMatrixGraph : public Graph_ADT<E, V>
 {
 public:
+    // Adjacency Matrix
     vector<vector<Edge<E, V> *>> matrix;
+
+    // map {ref_to_vertex : index in matrix}
     map<Vertex<V> *, int> vertex_index;
 
+    // map {name : ref}
+    map<string, Vertex<V> *> vertex_name;
+
+    /**
+     * @brief new vertex adding
+     * 
+     * @param value name of vertex
+     * @return Vertex<V>* ref to vertex
+     */
     Vertex<V> *addVertex(V value)
     {
         // new vertex creating
         auto new_vertex = new Vertex(value);
 
         // matrix resizing
-        matrix.resize(this->matrix.size() + 1);
+        matrix.resize(matrix.size() + 1);
         for (int i = 0; i < matrix.size(); ++i)
         {
-            matrix[i].resize(this->matrix.size());
+            matrix[i].resize(matrix.size());
         }
 
         // insertion in map
-        vertex_index.insert({new_vertex, this->matrix.size() - 1});
+        vertex_index.insert({new_vertex, matrix.size() - 1});
+        vertex_name.insert({new_vertex->value, new_vertex});
 
         return new_vertex;
     }
 
-    void removeVertex(Vertex<V> vertex)
+    /**
+     * @brief vertex removing
+     * 
+     * @param vertex ref to vertex
+     */
+    void removeVertex(Vertex<V> *vertex)
     {
+        int index = vertex_index.at(vertex);
+        for (int i = 0; i < matrix.size(); i++)
+        {
+
+            matrix[i][index] = nullptr;
+            matrix[index][i] = nullptr;
+        }
+
+        vertex_index.erase(vertex);
+        vertex_name.erase(vertex->value);
     }
 
+    /**
+     * @brief new edge adding
+     * 
+     * @param from ref to "from" vertex
+     * @param to ref to "to" vertex
+     * @param weight value of edge
+     * @return Edge<E, V>* ref to new edge
+     */
     Edge<E, V> *addEdge(Vertex<V> *from, Vertex<V> *to, E weight)
     {
         auto new_edge = new Edge<E, V>(from, to, weight);
@@ -95,34 +154,110 @@ public:
 
         matrix[index1][index2] = new_edge;
 
-        // TODO
-        //? matrix[index2][index1] = weight; reverse or not?
+        return matrix[index1][index2];
     }
 
-    void removeEdge(Edge<E, V> edge)
+    /**
+     * @brief edge removing
+     * 
+     * @param edge ref to edge
+     */
+    void removeEdge(Edge<E, V> *edge)
     {
+        int index1 = vertex_index.at(edge->begin);
+        int index2 = vertex_index.at(edge->end);
+
+        matrix[index1][index2] = nullptr;
     }
 
-    vector<Edge<E, V>> edgesFrom(Vertex<V> vertex)
+    /**
+     * @brief returns all vertices which starts from vertex
+     * 
+     * @param vertex ref to vertex
+     * @return vector<Edge<E, V> *> vector of all edges
+     */
+    vector<Edge<E, V> *> edgesFrom(Vertex<V> *vertex)
     {
+        vector<Edge<E, V> *> res;
+        int index = vertex_index.at(vertex);
+        for (int i = 0; i < matrix.size(); i++)
+        {
+            res.push_back(matrix[index][i]);
+        }
+
+        return res;
     }
 
-    vector<Edge<E, V>> edgesTo(Vertex<V> vertex)
+    /**
+     * @brief returns all vertices which ends in vertex
+     * 
+     * @param vertex ref to vertex
+     * @return vector<Edge<E, V> *> vector of all edges
+     */
+    vector<Edge<E, V> *> edgesTo(Vertex<V> *vertex)
     {
+        vector<Edge<E, V> *> res;
+        int index = vertex_index.at(vertex);
+        for (int i = 0; i < matrix.size(); i++)
+        {
+            res.push_back(matrix[i][index]);
+        }
+
+        return res;
     }
 
-    Vertex<V> findVertex(V value)
+    /**
+     * @brief vertex finding
+     * 
+     * @param value name of vertex
+     * @return Vertex<V>* ref to vertex
+     */
+    Vertex<V> *findVertex(V value)
     {
+        for (auto i : vertex_index)
+        {
+            if (i.first->value == value)
+            {
+                return i.first;
+            }
+        }
     }
 
-    Edge<E, V> findEdge(V from_value, V to_value)
+    /**
+     * @brief edge finding
+     * 
+     * @param from_value name of start vertex
+     * @param to_value name of end vertex
+     * @return Edge<E, V>* ref to edge
+     */
+    Edge<E, V> *findEdge(V from_value, V to_value)
     {
+        int index1 = vertex_index.at(vertex_name.at(from_value));
+        int index2 = vertex_index.at(vertex_name.at(to_value));
+
+        return matrix[index1][index2];
     }
 
-    bool hasEdge(Vertex<V> v, Vertex<V> u)
+    /**
+     * @brief check is there edge
+     * 
+     * @param v ref to first vertex
+     * @param u ref to second vertex
+     * @return true if yes
+     * @return false if not
+     */
+    bool hasEdge(Vertex<V> *v, Vertex<V> *u)
     {
+        int index1 = vertex_index.at(v);
+        int index2 = vertex_index.at(u);
+
+        return matrix[index1][index2] ? true : false;
     }
 
+    /**
+     * @brief prints matrix
+     * 
+     */
     void printMatrix()
     {
         for (int i = 0; i < matrix.size(); i++)
@@ -134,27 +269,92 @@ public:
             cout << endl;
         }
     }
-
-    void printSize()
-    {
-        cout << matrix.size() << endl;
-    }
 };
+
+/**
+ * @brief input parsing
+ * 
+ * @param line one line of input
+ * @return vector<string> line splited in words
+ */
+vector<string> parse(string line)
+{
+    vector<string> res;
+    string token = "";
+    for (auto i : line)
+    {
+
+        if (i == ' ')
+        {
+            res.push_back(token);
+            token = "";
+        }
+        else
+        {
+            token += i;
+        }
+    }
+    res.push_back(token);
+
+    return res;
+}
 
 int main(int argc, char const *argv[])
 {
     auto matrix = AdjacencyMatrixGraph<int, string>();
 
-    auto vertex1 = matrix.addVertex("govno1");
-    matrix.printSize();
-    matrix.printMatrix();
-    auto vertex2 = matrix.addVertex("govno2");
-    matrix.printSize();
-    matrix.printMatrix();
-    matrix.addEdge(vertex1, vertex2, 10);
-    matrix.printSize();
-    matrix.printMatrix();
-    cout << matrix.matrix[0][1]->weight;
+    string line = " ";
 
+    // parsing
+    while (!line.empty())
+    {
+        getline(cin, line);
+        auto instructions = parse(line);
+        if (instructions[0] == "ADD_VERTEX")
+        {
+            string name = instructions[1];
+            matrix.addVertex(name);
+        }
+        else if (instructions[0] == "REMOVE_VERTEX")
+        {
+            string name = instructions[1];
+            auto ref = matrix.vertex_name.at(name);
+            matrix.removeVertex(ref);
+        }
+        else if (instructions[0] == "ADD_EDGE")
+        {
+            string from = instructions[1];
+            string to = instructions[2];
+            int weight = stoi(instructions[3]);
+
+            auto ref1 = matrix.vertex_name.at(from);
+            auto ref2 = matrix.vertex_name.at(to);
+            matrix.addEdge(ref1, ref2, weight);
+        }
+        else if (instructions[0] == "REMOVE_EDGE")
+        {
+            string from = instructions[1];
+            string to = instructions[2];
+
+            matrix.removeEdge(matrix.findEdge(from, to));
+        }
+        else if (instructions[0] == "HAS_EDGE")
+        {
+            string from = instructions[1];
+            string to = instructions[2];
+
+            auto ref1 = matrix.vertex_name.at(from);
+            auto ref2 = matrix.vertex_name.at(to);
+
+            if (matrix.hasEdge(ref1, ref2))
+            {
+                cout << "TRUE" << endl;
+            }
+            else
+            {
+                cout << "FALSE" << endl;
+            }
+        }
+    }
     return 0;
 }
