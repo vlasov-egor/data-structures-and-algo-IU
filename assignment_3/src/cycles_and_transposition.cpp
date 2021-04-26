0505a0975581b102ef9ce28de4aef285cf90bd68
Egor Vlasov <vlasov05102002@gmail.com>
2021-04-26 13:21:59 +0300

    added comments

--- assignment_3/src/cycles_and_transposition.cpp
+++ assignment_3/src/cycles_and_transposition.cpp
@@ -130,14 +130,25 @@ public:
      */
     void removeVertex(Vertex<V> *vertex)
     {
-        int index = vertex_index.at(vertex);
-        for (int i = 0; i < matrix.size(); i++)
-        {
+        int index = vertex_index[vertex];
 
-            matrix[i][index] = nullptr;
-            matrix[index][i] = nullptr;
+        // decreasing the index by 1 if the vertex has an index greater than the current
+        for (auto &i : vertex_index)
+        {
+            if (i.second > index)
+            {
+                i.second -= 1;
+            }
         }
 
+        // removing from matrix row and column of current vertex
+        matrix.erase(matrix.begin() + index);
+        for (auto &i : matrix)
+        {
+            i.erase(i.begin() + index);
+        }
+
+        // removing from maps
         vertex_index.erase(vertex);
         vertex_name.erase(vertex->value);
     }
@@ -154,8 +165,8 @@ public:
     {
         auto new_edge = new Edge<E, V>(from, to, weight);
 
-        int index1 = vertex_index.at(from);
-        int index2 = vertex_index.at(to);
+        int index1 = vertex_index[from];
+        int index2 = vertex_index[to];
 
         matrix[index1][index2] = new_edge;
 
@@ -169,8 +180,8 @@ public:
      */
     void removeEdge(Edge<E, V> *edge)
     {
-        int index1 = vertex_index.at(edge->begin);
-        int index2 = vertex_index.at(edge->end);
+        int index1 = vertex_index[edge->begin];
+        int index2 = vertex_index[edge->end];
 
         matrix[index1][index2] = nullptr;
     }
@@ -184,11 +195,21 @@ public:
     vector<Edge<E, V> *> edgesFrom(Vertex<V> *vertex)
     {
         vector<Edge<E, V> *> res;
-        int index = vertex_index.at(vertex);
-        for (int i = 0; i < matrix.size(); i++)
+        int index = vertex_index[vertex];
+
+        for (auto vertex_pair : vertex_index)
         {
-            res.push_back(matrix[index][i]);
+            if (matrix[index][vertex_pair.second] != nullptr)
+            {
+                res.push_back(matrix[index][vertex_pair.second]);
+            }
         }
+
+        // for (int i = 0; i < matrix.size(); i++)
+        // {
+        //     if (matrix[index][i] != nullptr)
+        //         res.push_back(matrix[index][i]);
+        // }
 
         return res;
     }
@@ -202,7 +223,8 @@ public:
     vector<Edge<E, V> *> edgesTo(Vertex<V> *vertex)
     {
         vector<Edge<E, V> *> res;
-        int index = vertex_index.at(vertex);
+        int index = vertex_index[vertex];
+
         for (int i = 0; i < matrix.size(); i++)
         {
             res.push_back(matrix[i][index]);
@@ -238,8 +260,24 @@ public:
      */
     Edge<E, V> *findEdge(V from_value, V to_value)
     {
-        int index1 = vertex_index.at(vertex_name.at(from_value));
-        int index2 = vertex_index.at(vertex_name.at(to_value));
+        int index1 = vertex_index[vertex_name[from_value]];
+        int index2 = vertex_index[vertex_name[to_value]];
+
+        return matrix[index1][index2];
+    }
+
+    /**
+     * @brief edge finding
+     * 
+     * @param from start vertex
+     * @param to end vertex
+     * @return Edge<E, V>* ref to edge 
+     */
+    Edge<E, V> *findEdge(Vertex<V> *from, Vertex<V> *to)
+    {
+
+        int index1 = vertex_index[from];
+        int index2 = vertex_index[to];
 
         return matrix[index1][index2];
     }
@@ -254,8 +292,8 @@ public:
      */
     bool hasEdge(Vertex<V> *v, Vertex<V> *u)
     {
-        int index1 = vertex_index.at(v);
-        int index2 = vertex_index.at(u);
+        int index1 = vertex_index[v];
+        int index2 = vertex_index[u];
 
         return matrix[index1][index2] ? true : false;
     }
@@ -282,12 +320,12 @@ public:
      */
     vector<Vertex<V> *> isAcyclic()
     {
+        // map with visited indexes
         auto visited = new map<Vertex<V> *, bool>();
 
         for (auto vertex_pair : vertex_index)
         {
             auto current_vertex = vertex_pair.first;
-
             auto answer = dfs(visited, current_vertex, {});
 
             if (answer.size() != 0)
@@ -305,80 +343,89 @@ public:
      */
     void transpose()
     {
+        vector<Edge<E, V> *> edges_to_transpose;
 
-        if (matrix.size() != 0)
+        // swapping in adjacency matrix
+        for (auto vertex_pair1 : vertex_index)
         {
-            for (int i = 0; i < matrix.size(); i++)
+            for (auto vertex_pair2 : vertex_index)
             {
-                for (int j = i + 1; j < matrix.size(); j++)
+                int i = vertex_pair1.second;
+                int j = vertex_pair2.second;
+                if (matrix[i][j] != nullptr)
                 {
-                    if (matrix[i][j] != nullptr)
-                    {
-                        auto buffer1 = matrix[i][j]->begin;
-                        matrix[i][j]->begin = matrix[i][j]->end;
-                        matrix[i][j]->end = buffer1;
-                    }
-
-                    if (matrix[j][i] != nullptr)
-                    {
-                        auto buffer1 = matrix[j][i]->begin;
-                        matrix[j][i]->begin = matrix[j][i]->end;
-                        matrix[j][i]->end = buffer1;
-                    }
-
-                    auto buffer2 = matrix[i][j];
-                    matrix[i][j] = matrix[j][i];
-                    matrix[j][i] = buffer2;
+                    edges_to_transpose.push_back(matrix[i][j]);
+                    matrix[i][j] = nullptr;
                 }
             }
+        }
+
+        // swapping begin and end in edge
+        for (auto edge : edges_to_transpose)
+        {
+            auto i = vertex_index[edge->begin];
+            auto j = vertex_index[edge->end];
+
+            auto temp = edge->begin;
+            edge->begin = edge->end;
+            edge->end = temp;
+
+            matrix[j][i] = edge;
         }
     }
 
 private:
+    /**
+     * @brief 
+     * 
+     * @param visited bool map with visited vertices
+     * @param current current vertex
+     * @param path path of cycle 
+     * @return vector<Vertex<V> *> our path
+     */
     vector<Vertex<V> *> dfs(map<Vertex<V> *, bool> *visited,
                             Vertex<V> *current, vector<Vertex<V> *> path)
     {
 
+        // if already visited
         if (visited->find(current) != visited->end())
             return {};
 
+        // mark it visited
         visited->insert({current, true});
 
+        // check all edges which connected with current
         for (auto edge : edgesFrom(current))
         {
-            if (edge != nullptr)
+            auto next_vertex = edge->end;
+
+            if (find(path.begin(), path.end(), next_vertex) != path.end())
             {
+                // cycle searching
+                vector<Vertex<V> *> cycle;
+                path.push_back(current);
 
-                auto next_vertex = edge->end;
-
-                if (find(path.begin(), path.end(), next_vertex) != path.end())
+                while (path.back() != next_vertex)
                 {
-
-                    vector<Vertex<V> *> cycle;
-                    path.push_back(current);
-
-                    while (path.back() != next_vertex)
-                    {
-                        cycle.push_back(path.back());
-                        path.pop_back();
-                    }
-                    cycle.push_back(next_vertex);
-
-                    reverse(cycle.begin(), cycle.end());
-
-                    return cycle;
+                    cycle.push_back(path.back());
+                    path.pop_back();
                 }
+                cycle.push_back(next_vertex);
 
-                if (visited->find(next_vertex) == visited->end())
-                {
-                    auto tmp_path = path;
-                    tmp_path.push_back(current);
+                reverse(cycle.begin(), cycle.end());
 
-                    auto result = dfs(visited, next_vertex, tmp_path);
+                return cycle;
+            }
 
-                    if (result.size() != 0)
-                        return result;
-                }
+            if (visited->find(next_vertex) == visited->end())
+            {
+                auto tmp_path = path;
+                tmp_path.push_back(current);
+
+                auto result = dfs(visited, next_vertex, tmp_path);
+
+                if (result.size() != 0)
+                    return result;
             }
         }
 
@@ -416,6 +463,7 @@ vector<string> parse(string line)
 
 int main(int argc, char const *argv[])
 {
+
     auto matrix = AdjacencyMatrixGraph<int, string>();
 
     string line = " ";
@@ -433,7 +481,7 @@ int main(int argc, char const *argv[])
         else if (instructions[0] == "REMOVE_VERTEX")
         {
             string name = instructions[1];
-            auto ref = matrix.vertex_name.at(name);
+            auto ref = matrix.vertex_name[name];
             matrix.removeVertex(ref);
         }
         else if (instructions[0] == "ADD_EDGE")
@@ -442,8 +490,8 @@ int main(int argc, char const *argv[])
             string to = instructions[2];
             int weight = stoi(instructions[3]);
 
-            auto ref1 = matrix.vertex_name.at(from);
-            auto ref2 = matrix.vertex_name.at(to);
+            auto ref1 = matrix.vertex_name[from];
+            auto ref2 = matrix.vertex_name[to];
             matrix.addEdge(ref1, ref2, weight);
         }
         else if (instructions[0] == "REMOVE_EDGE")
@@ -458,8 +506,8 @@ int main(int argc, char const *argv[])
             string from = instructions[1];
             string to = instructions[2];
 
-            auto ref1 = matrix.vertex_name.at(from);
-            auto ref2 = matrix.vertex_name.at(to);
+            auto ref1 = matrix.vertex_name[from];
+            auto ref2 = matrix.vertex_name[to];
 
             if (matrix.hasEdge(ref1, ref2))
             {
@@ -490,53 +538,20 @@ int main(int argc, char const *argv[])
                 string answer = "";
                 for (int i = 0; i < result.size() - 1; i++)
                 {
-                    auto v1 = result[i]->value;
-                    auto v2 = result[i + 1]->value;
+                    auto v1 = result[i];
+                    auto v2 = result[i + 1];
                     weight += matrix.findEdge(v1, v2)->weight;
 
-                    answer = answer + " " + v1;
+                    answer = answer + " " + v1->value;
                 }
                 weight +=
-                    matrix.findEdge(result[result.size() - 1]->value, result[0]->value)->weight;
+                    matrix.findEdge(result[result.size() - 1], result[0])->weight;
                 answer = answer + " " + result[result.size() - 1]->value;
                 answer = to_string(weight) + answer;
                 cout << answer << endl;
             }
         }
-        // else
-        // {
-        //     matrix.printMatrix();
-        // }
     }
 
     return 0;
 }
-
-/*
-ADD_VERTEX D
-ADD_VERTEX A
-ADD_VERTEX B
-ADD_VERTEX C
-ADD_EDGE A B 3
-ADD_EDGE B C 4
-TRANSPOSE
-ADD_EDGE A C 5
-IS_ACYCLIC
-
-
-TRANSPOSE
-ADD_VERTEX T
-HAS_EDGE T T
-ADD_VERTEX Z
-ADD_EDGE Z T 6
-ADD_VERTEX BB
-ADD_EDGE Z BB 90
-ADD_VERTEX C
-ADD_EDGE T Z -18
-ADD_EDGE BB C -97
-ADD_EDGE T C -52
-ADD_EDGE C BB -35
-ADD_EDGE BB T -83
-IS_ACYCLIC
-IS_ACYCLIC
-*/
